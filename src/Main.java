179e0450b02eb006e7e4850b0ae2b0534880ea34
lekshey9914@gmail.com <lekshey9914@gmail.com>
2023-02-27 22:53:27 -0500

    amazon

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class amazon{
+    public static void main(String[] args){
+
+        System.setProperty("Webdriver.chrome.driver","chromedriver");
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().fullscreen();
+
+
+
+         System.out.println("hello");
+
+
+
+    }
+}
